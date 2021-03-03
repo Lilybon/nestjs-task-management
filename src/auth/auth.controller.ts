136fa72a3d5a5f7a76d45f3ef20021bb55a9cab7
Lilybon <zebra10029@gmail.com>
2021-03-03 22:20:07 +0800

    feat: complete sign-in and basic jwt authentication for auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,8 +7,14 @@ export class AuthController {
   constructor(
     private authService: AuthService
   ) {}
+
   @Post('/signup')
   signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
     return this.authService.signUp(authCredentialsDto);
+  }
+
+  @Post('/signin')
+  signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
+    return this.authService.signIn(authCredentialsDto);
   }
 }
